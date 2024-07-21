568625d219b7cb1d4f186d258568e6eb8dfd93bf
Rafael Benite Gil <rafaelbgil@gmail.com>
2024-07-21 11:09:01 -0300

    removendo key padrao do settings.py

--- healthtech/healthtech/settings.py
+++ healthtech/healthtech/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from os import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--x$$$c$t^0%9nix@me8tpygcg$b7(l+sic7_0f@f6g^3%dnkjv'
+SECRET_KEY = environ.get['DJANGO_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
